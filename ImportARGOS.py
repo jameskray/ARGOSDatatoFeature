ca71e8ec6e9a4c514e0fa9155d71abc4b34c44c9
jameskray <42757617+jameskray@users.noreply.github.com>
2018-10-15 12:09:23 -0400

    Initial commit of ARGOS script

---
+++ ImportARGOS.py
@@ -0,0 +1,12 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a point
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2018
+## Author: james.k.ray@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
